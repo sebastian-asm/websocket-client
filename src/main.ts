066e4de5f50fc2d505e0286bf0ae86f890cdaf37
Sebastian Sanchez <sasanchezmiles@teco.com.ar>
2023-06-09 16:01:44 -0300

    listado de clientes conectados

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import './style.css'
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <h1>NestJS + WebSocket</h1>
+    <h4>Estado del Servidor: <span id="server-status"></span></h4>
+    <p><em>Clientes conectados</em></p>
+    <ul id="clients-connected"></ul>
   </div>
 `
 
--- src/socket-client.ts
+++ src/socket-client.ts
@@ -1,9 +1,25 @@
-import { Manager } from 'socket.io-client'
+import { Manager, Socket } from 'socket.io-client'
 
 export const connectToServer = () => {
   const url = 'http://localhost:3001/socket.io/socket.io.js'
   const manager = new Manager(url)
   // el / representa el namespace, este caso el root
   const socket = manager.socket('/')
-  console.log({ socket })
+  serverStatus(socket)
+}
+
+const serverStatus = (socket: Socket) => {
+  // ! indica que es un elemento que siempre va a existir
+  const status = document.querySelector('#server-status')!
+  const clientsList = document.querySelector('#clients-connected')!
+
+  // on: escuchar eventos del servidor
+  // emit: "hablarle" al servidor
+  socket.on('connect', () => (status.textContent = 'Conectado'))
+  socket.on('disconnect', () => (status.textContent = 'Desconectado'))
+  socket.on('clients-updated', (clients: string[]) => {
+    let addClient = ''
+    for (const client of clients) addClient += `<li>${client}</li>`
+    clientsList.innerHTML = addClient
+  })
 }
